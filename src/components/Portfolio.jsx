145a39828d4669d372ae375fbcf0a27ff89a4089
vanessathi <phamxvanessa@gmail.com>
2021-02-22 22:24:37 -0700

    added nav to portfolio page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Navbar from './Navbar';
+
 
 const Portfolio = () => {
     return (
-        <div className="Portfolio">
-             This is the portfolio page.
+        <div>
+
+        <Navbar />
+
+        <div class="hero">
+        <section class="hero is-large">
+            <div class="hero-body">
+                {/* <img src={HeroOne} draggable="false" alt='heroone' /> */}
+            </div>
+        </section>
+        </div>
+
         </div>
     );
 }
