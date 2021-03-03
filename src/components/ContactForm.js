cc08ad268ef50a28fbe8e5efc7680ba1af2628d5
vanessathi <phamxvanessa@gmail.com>
2021-03-02 23:12:05 -0700

    wrote functions for contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,6 +11,32 @@ class ContactForm extends Component {
             status: "Submit"
         };
     }
+    handleChange(event) {
+        const field = event.target.id;
+        if (field === "name") {
+            this.setState({ name: event.target.value});
+        } else if (field === "email") {
+            this.setState({ email: event.target.value });
+        } else if ( field === "message") {
+            this.setState({ message: event.target.value});
+        }
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({ status: "Sending "});
+        axios({
+            method: "POST",
+            url: "http://localhost:5000/contact",
+            data: this.state,
+        }).then((response) => {
+            if (response.data.status === "sent") {
+                alert("Message Sent!");
+                this.setState({ name:"", email:"", message:"", status:"Submit"});
+            } else if (response.data.status==="failed") {
+                alert("Message Failed");
+            }
+        });
+    }
     render () {
         let buttonText = this.state.status;
         return (
