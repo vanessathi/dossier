549f81dcb72c6906022010411f6e555abba3d88b
vanessathi <phamxvanessa@gmail.com>
2021-03-03 22:37:44 -0700

    layout for contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,13 @@ class Contact extends Component {
             <div>
             <Navbar />
             <div className={classes.Content}>
-            This is the contact page.
+            This is the contact page. Insert contact hero.
+            </div>
+            <div>
+                Insert contact form.
+            </div>
+            <div>
+                Insert footer here.
             </div>
             </div>
         )
